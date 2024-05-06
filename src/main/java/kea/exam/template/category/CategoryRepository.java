a32fd1c5c63e8d6189200b39bb540994f3652f24
AliHMohammad <alihmohammad@hotmail.com>
2024-05-06 14:59:50 +0200

    feat: created repositories for Category and Type

---
+++ src/main/java/kea/exam/template/category/CategoryRepository.java
@@ -0,0 +1,6 @@
+package kea.exam.template.category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
