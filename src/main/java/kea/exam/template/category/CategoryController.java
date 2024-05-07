f098ae292421012be04bd80fd9f5046bf58561a1
Forkeh <forkeh@gmail.com>
2024-05-07 15:03:49 +0200

    bugfix: changed mapping url

--- src/main/java/kea/exam/template/category/CategoryController.java
+++ src/main/java/kea/exam/template/category/CategoryController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("category")
+@RequestMapping("categories")
 public class CategoryController {
 
 
